9ec6a1ce68b8f31470f145bd8b48c1afa404bd5e
Meya Warrior <meyawarrior@gmail.com>
2025-09-17 01:58:13 -0500

    adding things

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,7 +1,20 @@
-function ModalWithForm() {
+import "./ModalWithForm.css";
+
+function ModalWithForm({ children, buttonText, title }) {
     return (
      <div className="modal">
-      <form className="modal__form"></form>  
+      <div className="modal__content"> 
+      <h2 className="modal__title">{title}</h2>
+      <button type="button" className="modal__close">
+        CLOSE
+      </button>
+      <form className="modal__form">  
+        {children}
+      <button type="submit" className="modal__submit">
+       {buttonText}
+      </button>
+      </form>
+      </div>
      </div>   
     );
 }
