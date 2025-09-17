93e3fc7f698e9f82967222be3bb42d37098fff11
Meya Warrior <meyawarrior@gmail.com>
2025-09-16 22:37:12 -0500

    correcting errors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import "./App.css";
 import Header from "./Header/Header.jsx";
 import Main from "../components/Main/Main.jsx";
+import ModalWithForm from "./ModalWithForm/ModalWithForm.jsx";
 
 function App() {
   const [weatherData, setWeatherData] = useState({ type: "cold" });
@@ -13,6 +14,7 @@ function App() {
     <Header />
     <Main weatherData={weatherData} />
     </div>
+    <ModalWithForm />
   </div>
   );
 }
---
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -0,0 +1,9 @@
+function ModalWithForm() {
+    return (
+     <div className="modal">
+      <form className="modal__form"></form>  
+     </div>   
+    );
+}
+
+export default ModalWithForm;
