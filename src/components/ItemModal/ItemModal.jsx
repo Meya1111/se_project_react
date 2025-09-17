b2d6d87119832609477cd77f7aa33e502882f906
Meya Warrior <meyawarrior@gmail.com>
2025-09-17 16:55:27 -0500

    adding things

---
+++ src/components/ItemModal/ItemModal.jsx
@@ -0,0 +1,23 @@
+import "./ItemModal.css"
+
+function ItemModal({ activeModal,onClose, Card }) {
+return (
+ <div className={`modal ${activeModal === "preview" && "modal_opened"}`}>
+   <div className="modal__content modal__content_type_image">
+   <button
+       onClick={onClose}
+       type="button"
+        className="modal__close">
+        CLOSE
+      </button>
+      <img src={Card.link} alt="" className="modal__image" />
+      <div className="modal__footer">
+        <h2 className="modal__caption">{Card.name}</h2>
+        <p className="modal__weather">Weather: {Card.weather}</p>
+      </div>
+    </div> 
+ </div>
+);
+}
+
+export default ItemModal;
